c8aac94cd41e592d97a878a49c4e56da938cf20a
Analice-cmd <silva.analice08@aluno.ifce.edu.br>
2026-09-25 15:03:24 -0300

    Add variable for project name and update output value

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
 }
+variable "nome_projeto" {
+description = "Nome do projeto Terraform"
+default = "meu primeirissimo projeto com variavel no Terraform"
+}
+
+
 output "mensagem" {
-  value = "MEU PRIMEIRO PROJETO TERRAFORM CHUPA BOSSS(O)"
+  value = var.nome_projeto
 }
